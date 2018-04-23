ec271a3663c21f4ddb9d5f6e391b003007258e58
kisulken <daniel.furmanov@gmail.com>
2018-04-22 23:42:58 -0400

    added plot for display db's data

---
+++ plot_orders_history.py
@@ -0,0 +1,28 @@
+import sqlite3
+
+import matplotlib.pyplot as plt
+
+
+def quick_plot_from_sql(set1, set2, title='X/Y', xlb='X', ylb='Y'):
+    # converting a set of tuples to a sorted list
+    list1 = list(set1)
+    list2 = list(set2)
+    list1.sort(reverse=True)
+    list2.sort(reverse=True)
+    # plotting
+    plt.title(title)
+    plt.plot(list1, list2)
+    plt.axis([list1[len(list1) - 1], list1[0],
+              list2[len(list2) - 1], list2[0]])
+    plt.xlabel(xlb)
+    plt.ylabel(ylb)
+    plt.show()
+
+
+conn = sqlite3.connect('stocks.db')
+c = conn.cursor()
+sql = "select size, sizemul from stocks where sym1='XRP'"
+c.execute(sql)
+result = c.fetchall()
+set1, set2 = zip(*result)
+quick_plot_from_sql(set1, set2)
